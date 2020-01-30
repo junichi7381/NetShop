3bd5615cac5925d927c4faf0d50e7ab3f87d2a98
junichi7381 <57358337+junichi7381@users.noreply.github.com>
2020-01-30 20:30:45 +0900

    ユーザー登録機能完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_28_212238) do
+ActiveRecord::Schema.define(version: 2020_01_30_111004) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "item_name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_01_28_212238) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
